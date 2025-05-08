be170f9031cc228874f14a07751f343445f60f98
Eason Zhang <799879553@qq.com>
2025-05-08 15:05:09 +0800

    feat: add global abort controller for chat streaming

--- src/typings/global.d.ts
+++ src/typings/global.d.ts
@@ -3,4 +3,6 @@ interface Window {
   $dialog?: import('naive-ui').DialogProviderInst
   $message?: import('naive-ui').MessageProviderInst
   $notification?: import('naive-ui').NotificationProviderInst
+  // Global abort controller for chat streaming
+  controller?: AbortController
 }
